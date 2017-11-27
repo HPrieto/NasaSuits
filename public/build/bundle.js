49eabadd0ad2e0e945da748a3f6549d1021d7431
Heriberto Prieto <duskayame@gmail.com>
2017-11-27 12:59:35 -0800

    react/express backend complete

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -1021,7 +1021,12 @@ var App = function (_Component) {
                 'div',
                 null,
                 _react2.default.createElement(_Navbar2.default, null),
-                _react2.default.createElement(_Home2.default, null)
+                _react2.default.createElement(_Home2.default, null),
+                _react2.default.createElement(
+                    'div',
+                    null,
+                    'hello'
+                )
             );
         }
     }]);
